14a512e45071dce0fef27a9fbcaf907c5c72b271
URA, Takefumi <ura.takefumi@gmail.com>
2021-04-27 00:30:41 +0900

    [feat] import original rake_ziphandler

--- lib/rake_ziphandler.rb
+++ lib/rake_ziphandler.rb
@@ -2,7 +2,83 @@
 
 require_relative "rake_ziphandler/version"
 
-module RakeZiphandler
-  class Error < StandardError; end
-  # Your code goes here...
+class RakeZipHandler
+  #  prefix: zipファイル名の先頭固定部分
+  #  content: zipに入れる内容
+  #  zipdir: zipを作成するディレクトリ
+  #  remote_path: 同期先のパス (rsync形式)
+  #  zipopt: zip生成時につけるオプション (`-x .DS_Store -r`)
+  #  nremains: 古いzipを残す数 (`2`)
+  #  depend_on: makeが依存するタスクを指定 -- sweep_macbinary等を想定 (`[]`)
+  #  after_deploy: deploy後に実行するコード。selfを渡す。
+  def initialize(prefix:,
+                 content:,
+                 zipdir:,
+                 remote_path:,
+                 namespace: :zip,
+                 zipopt: '-x .DS_Store -r',
+                 nremains: 2,
+                 depend_on: [],
+                 after_deploy: ->(_self) {})
+    @prefix = prefix
+    @content = content
+    @zipdir = zipdir
+    @remote_path = remote_path
+    @namespace = namespace.intern
+    @zipopt = zipopt
+    @nremains = nremains
+    @depend_on = depend_on
+    @after_deploy = after_deploy
+
+    @zipname = Time.now.strftime("#{@prefix}-%y%m%d-%H%M.zip")
+    @zippath = File.join(@zipdir, @zipname)
+
+    define_task
+  end
+  attr_reader :zipname
+
+  def list
+    Dir[File.join(@zipdir, "#{@prefix}-*.zip")].sort
+  end
+
+  def primary_zipname
+    File.basename(list.last)
+  end
+
+  private
+
+  def define_task
+    extend Rake::DSL
+
+    namespace @namespace do
+      directory @zipdir
+
+      desc "create a zip file"
+      task make: [*@depend_on, @zipdir] do
+        srcdir = File.dirname(@content)
+        impdir = File.basename(@content)
+        Dir.chdir(srcdir) do
+          cmd = "zip #{@zipopt} #{@zippath} #{impdir}"
+          sh cmd
+        end
+      end
+
+      desc "sweep old zip files"
+      task sweep: [@zipdir] do
+        zipfiles = list
+        zipfiles.pop(@nremains)
+        File.delete(*zipfiles)
+      end
+
+      desc "sync zip files with remote directory"
+      task deploy: [] do
+        sh "rm -f #{@zipdir}/.DS_Store"
+        sh "rsync -av --delete #{@zipdir}/ #{@remote_path}"
+        @after_deploy.call(self)
+      end
+
+      desc "zip deploy suite -- sweep -> make -> deploy"
+      task deploy_suite: ["#{@namespace}:sweep", "#{@namespace}:make", "#{@namespace}:deploy"]
+    end
+  end
 end
--- test/rake_ziphandler_test.rb
+++ test/rake_ziphandler_test.rb
@@ -1,13 +1,45 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require_relative "../lib/rake_ziphandler"
 
-class RakeZiphandlerTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::RakeZiphandler::VERSION
-  end
+module RakeZipHandlerTest
+  def self.test(_argv)
+    flag_called_task_foo = false
+    Rake::Task.define_task(:default) # テスト時にデフォルトタスクがないとrakeがabortする
+    Rake::Task.define_task(:foo) do
+      flag_called_task_foo = true
+    end
 
-  def test_it_does_something_useful
-    assert false
+    testdir = File.join(__dir__, 'test/site')
+    zipdir = File.join(__dir__, 'test/zip')
+    prefix = 'foo'
+    system("mkdir -p #{testdir} ; echo hello > #{testdir}/test.txt")
+    FileUtils.mkdir_p(zipdir)
+
+    _z = RakeZipHandler.new(prefix: prefix, content: testdir, zipdir: zipdir, remote_path: '', depend_on: [:foo])
+
+    # test sweep
+    FileUtils.touch "#{zipdir}/#{prefix}-200101-1212.zip"
+    FileUtils.touch "#{zipdir}/#{prefix}-200101-1213.zip"
+    FileUtils.touch "#{zipdir}/#{prefix}-200101-1214.zip"
+    Rake::Task['zip:sweep'].invoke
+    ls = Dir[File.join(zipdir, '*')]
+
+    raise "sweep後zipファイルの数は2個でなければならない" if ls.size != 2
+
+    %w(1213.zip 1214.zip).each do |fn|
+      raise "sweep #{fn} が消えている" unless ls.find {|f| f =~ /#{fn}$/}
+    end
+
+    Rake::Task['zip:make'].invoke
+    raise "make前にタスクfooが実行されていなければならない" unless flag_called_task_foo
+  ensure
+    system("rm -rf #{File.join(__dir__, 'test')}")
   end
 end
+
+RakeZipHandlerTest.test(ARGV)
+
+# Local Variables:
+# compile-command: "rake -f rake_ziphandler_test.rb"
+# End:
